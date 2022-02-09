3f534509485ffbeab478dc708fee78b73c6b3eda
Koen Jacoby <kjacoby3@ycp.edu>
2022-02-09 16:59:57 -0500

    Initial Commit

---
+++ CS320_Lab02a_kjacoby3/junit/edu/ycp/cs320/lab02/controller/GuessingGameControllerTest.java
@@ -0,0 +1,52 @@
+package edu.ycp.cs320.lab02.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ycp.cs320.lab02a.controller.GuessingGameController;
+import edu.ycp.cs320.lab02a.model.GuessingGame;
+
+public class GuessingGameControllerTest {
+	private GuessingGame model;
+	private GuessingGameController controller;
+	
+	@Before
+	public void setUp() {
+		model = new GuessingGame();
+		controller = new GuessingGameController();
+		
+		model.setMin(1);
+		model.setMax(100);
+		
+		controller.setModel(model);
+	}
+	
+	@Test
+	public void testNumberIsGreater() {
+		int currentGuess = model.getGuess();
+		controller.setNumberIsGreaterThanGuess();
+		assertTrue(model.getGuess() > currentGuess);
+	}
+	
+	@Test
+	public void testNumberIsLess() {
+		int currentGuess = model.getGuess();
+		controller.setNumberIsLessThanGuess();
+		assertTrue(model.getGuess() < currentGuess);
+	}
+	
+	@Test
+	public void testNumberFound() {
+		controller.setNumberFound();
+		assertTrue(model.isDone());
+	}
+	
+	@Test
+	public void testStartGame() {
+		controller.startGame();
+		assertTrue(model.getMax() == 100);
+		assertTrue(model.getMin() == 1);
+	}
+}
