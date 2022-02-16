a1af09826b4a3571ad28991dc30f2d59b57156a5
kjacoby3 <kjacoby3@ycp.edu>
2022-02-16 10:42:45 -0500

    Index Page Changes Completed

--- CS320_Lab02a_kjacoby3/src/edu/ycp/cs320/lab02a/servlet/IndexServlet.java
+++ CS320_Lab02a_kjacoby3/src/edu/ycp/cs320/lab02a/servlet/IndexServlet.java
@@ -18,4 +18,24 @@ public class IndexServlet extends HttpServlet {
 		
 		req.getRequestDispatcher("/_view/index.jsp").forward(req, resp);
 	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		System.out.println("Index Servlet: doPost");
+		
+		if (req.getParameter("addNumbers") != null) {
+			req.getRequestDispatcher("/_view/addNumbers.jsp").forward(req, resp);
+		}
+		
+		if (req.getParameter("multiplyNumbers") != null) {
+			req.getRequestDispatcher("/_view/multiplyNumbers.jsp").forward(req, resp);
+		}
+		
+		if (req.getParameter("guessingGame") != null) {
+			req.getRequestDispatcher("/_view/guessingGame.jsp").forward(req, resp);
+		}
+	}
+	
 }
--- CS320_Lab02a_kjacoby3/war/_view/index.jsp
+++ CS320_Lab02a_kjacoby3/war/_view/index.jsp
@@ -7,5 +7,18 @@
 
 	<body>
 		This is the index view jsp
+		<form action="${pageContext.servletContext.contextPath}/index" method="post">
+			<table>
+				<tr>
+					<td><input name="addNumbers" type="submit" value="Add Numbers"></td>
+				</tr>
+				<tr>
+					<td><input name="multiplyNumbers" type="submit" value="Multiply Numbers"></td>
+				</tr>
+				<tr>
+					<td><input name="guessingGame" type="submit" value="Guessing Game"></td>
+				</tr>
+			</table>
+		</form>
 	</body>
 </html>
